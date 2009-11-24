e24cb83aa7f7685a4811e5c8d39541bae7b15cb2
Christian Fowler <spider@viovio.com>
2009-11-24 22:26:09 +0000

    import

---
+++ bit_setup_inc.php
@@ -0,0 +1,18 @@
+<?php
+global $gBitSystem, $gBitSmarty, $gBitThemes;
+
+$registerHash = array(
+	'package_name' => 'ckeditor',
+	'package_path' => dirname( __FILE__ ).'/',
+);
+
+$gBitSystem->registerPackage( $registerHash );
+
+// Add our plugin directory.
+$gBitSmarty->plugins_dir[] = $registerHash['package_path']."smarty";
+
+if( $gBitSystem->isPackageActive( 'ckeditor' ) && $gBitUser->hasPermission( 'p_liberty_enter_html' ) ){
+	$gBitThemes->loadJavascript( CKEDITOR_PKG_PATH.'jscripts/ckeditor.js' );
+}
+
+?>
