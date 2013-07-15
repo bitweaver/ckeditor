6d4c4f12d15ae68d912972921997a8c5180c9aea
lsces <lester@lsces.co.uk>
2013-07-15 14:22:23 +0100

    Upgrade to CKEditor V4 Directory structure changed so many files removed and replace in a new location This batch is the delete and add

---
+++ plugins/colordialog/lang/gl.js
@@ -0,0 +1,11 @@
+﻿/*
+Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang( 'colordialog', 'gl', {
+	clear: 'Limpar',
+	highlight: 'Resaltar',
+	options: 'Opcións de cor',
+	selected: 'Cor seleccionado',
+	title: 'Seleccione unha cor'
+});
---
+++ plugins/colordialog/lang/ku.js
@@ -0,0 +1,11 @@
+﻿/*
+Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang( 'colordialog', 'ku', {
+	clear: 'پاکیکەوە',
+	highlight: 'نیشانکردن',
+	options: 'هەڵبژاردەی ڕەنگەکان',
+	selected: 'ڕەنگی هەڵبژێردراو',
+	title: 'هەڵبژاردنی ڕەنگ'
+});
