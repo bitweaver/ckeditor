1ccc5d9b0c53cba895f2dc11d6115bdd416e8e92
spider <spider@bitweaver.org>
2015-02-05 00:23:05 -0500

    move load to a function, and load jquery adapter

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -8,10 +8,18 @@ $registerHash = array(
 
 $gBitSystem->registerPackage( $registerHash );
 
-if( $gBitSystem->isPackageActive( 'ckeditor' ) && $gBitUser->hasPermission( 'p_liberty_enter_html' ) ){
-	if( defined( 'IS_LIVE' ) && IS_LIVE ) {
-		$gBitThemes->loadJavascript( CKEDITOR_PKG_PATH.'ckeditor/ckeditor.js', FALSE, 600, FALSE );
-	} else {
-		$gBitThemes->loadJavascript( CKEDITOR_PKG_PATH.'ckeditor/ckeditor.js', FALSE, 600, FALSE );
+function loadCkEditor() {
+	global $gBitSystem, $gBitThemes, $gBitUser;
+	if( $gBitSystem->isPackageActive( 'ckeditor' ) && $gBitUser->hasPermission( 'p_liberty_enter_html' ) ){
+		// same files for now... should choose between .min files
+		if( defined( 'IS_LIVE' ) && IS_LIVE ) {
+			$jsDir = (file_exists( CONFIG_PKG_PATH.'externals/ckeditor/ckeditor.js' ) ? CONFIG_PKG_PATH.'externals/ckeditor/' : CKEDITOR_PKG_PATH.'ckeditor/');
+			$gBitThemes->loadJavascript( $jsDir.'ckeditor.js', FALSE, 600, FALSE );
+			$gBitThemes->loadJavascript( $jsDir.'adapters/jquery.js', FALSE, 600, FALSE );
+		} else {
+			$jsDir = (file_exists( CONFIG_PKG_PATH.'externals/ckeditor/ckeditor.js' ) ? CONFIG_PKG_PATH.'externals/ckeditor/' : CKEDITOR_PKG_PATH.'ckeditor/');
+			$gBitThemes->loadJavascript( $jsDir.'ckeditor.js', FALSE, 600, FALSE );
+			$gBitThemes->loadJavascript( $jsDir.'adapters/jquery.js', FALSE, 600, FALSE );
+		}
 	}
 }
